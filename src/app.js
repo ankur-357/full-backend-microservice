47cf6c59f3bbfdfe8c8985d4def5bd6e8f5460d5
Ankur Kumar <ankurtiwari502@gmail.com>
2025-05-26 20:44:31 +0530

    new change

--- src/app.js
+++ src/app.js
@@ -93,15 +93,6 @@ app.use(
   })
 );
 
-// Serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
